9a8f03ac3366cd5806ea697b93853dfe7a96bc03
Javier Rojas <crojas@lcgej.unam.mx>
2021-08-18 09:12:22 -0500

    Problem 3 of Rosalind

---
+++ Rosalind3.py
@@ -0,0 +1,10 @@
+s= input("Ingrese el string que desee dividir de maximo 200 letras entre comillas")
+
+a= input( "Ingrese el numero a partir de el cual desee imprimir a pantalla" )
+b= input( "Ingrese el numero hasta donde desea imprimir a pantalla" )
+c= input( "Ingrese el numero a partir del segundo bloque que desee imprimir a pantalla" )
+d= input( "Ingrese el numero hasta donde desea imprimir a pantalla" )
+
+
+print s[a : b+1]
+print s[c : d+1]
